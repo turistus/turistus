b2dbccd08b61269a0420d08360eb4355fa60ba73
turistus <desconectaprincipal@hotmail.com>
2023-03-26 13:52:08 -0300

    3 Conections alterados

--- adm/connection.php
+++ adm/connection.php
@@ -5,28 +5,27 @@ if(!defined('ACCESS')){
     die("Erro: Pagina nao encontrada!<br>");
 }
 
+/*
 $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "empresa";
 $port = 3306;
-
-/*
-
-$host = "mysql743.umbler.com";
-$user = "turismo2021";
-$pass = "turismo123";
-$dbname = "bdtur";
-
 */
+
+
+$host = "mysql";
+$user = "u494298311_36232219880";
+$pass = "Senha@123";
+$dbname = "u494298311_tur";
 
 try {
 
 
 
     $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
-    
-    //conex para conectar de outro modo e poder listar com paginação. 
+
+    //conex para conectar de outro modo e poder listar com paginação.
     $conex = mysqli_connect($host, $user, $pass, $dbname);
     //echo "OK BD";
     //echo "Conexão com banco de dados realizado com sucesso!<br>";
--- lgn/connection.php
+++ lgn/connection.php
@@ -5,25 +5,24 @@ if(!defined('ACCESS')){
     die("Erro: Pagina nao encontrada!<br>");
 }
 
+/*
 $host = "localhost";
 $user = "root";
 $pass = "";
 $dbname = "empresa";
 $port = 3306;
-
-/*
-
-$host = "mysql743.umbler.com";
-$user = "turismo2021";
-$pass = "turismo123";
-$dbname = "bdtur";
-
 */
+
+
+$host = "mysql";
+$user = "u494298311_36232219880";
+$pass = "Senha@123";
+$dbname = "u494298311_tur";
 
 try {
 
 
-   
+
  	//$conn = new PDO("mysql:host=$host;dbname=" . $dbname, $user, $pass);
 
     $conn = new PDO("mysql:host=$host;port=$port;dbname=" . $dbname, $user, $pass);
