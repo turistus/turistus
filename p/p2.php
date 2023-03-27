a27c51c18ca0653eff5158132d05835549ebf3e4
turistus <desconectaprincipal@hotmail.com>
2023-03-27 01:15:05 -0300

    P

--- p/p2.php
+++ p/p2.php
@@ -263,11 +263,11 @@ $idGuia = filter_input(INPUT_GET, "idGuia", FILTER_SANITIZE_NUMBER_INT);
 
                         <input type="hidden" name="billingAddressCountry" id="billingAddressCountry" value="BRL">
 
-                        <input type="hidden" name="receiverEmail" id="receiverEmail" value="<?php echo EMAIL_LOJA; ?>">
+                        <input type="hidden" name="receiverEmail" id="receiverEmail" value="<?php echo $EMAIL_LOJA; ?>">
 
-                        <input type="hidden" name="currency" id="currency" value="<?php echo MOEDA_PAGAMENTO; ?>">
+                        <input type="hidden" name="currency" id="currency" value="<?php echo $MOEDA_PAGAMENTO; ?>">
 
-                        <input type="hidden" name="notificationURL" id="notificationURL" value="<?php echo URL_NOTIFICACAO; ?>">
+                        <input type="hidden" name="notificationURL" id="notificationURL" value="<?php echo $URL_NOTIFICACAO; ?>">
 
 
                         <input type="hidden" name="reference" id="reference" value="<?php echo $row_car['id'] ?>">
@@ -293,7 +293,7 @@ $idGuia = filter_input(INPUT_GET, "idGuia", FILTER_SANITIZE_NUMBER_INT);
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
 
-        <script type="text/javascript" src="<?php echo SCRIPT_PAGSEGURO; ?>"></script>
+        <script type="text/javascript" src="<?php echo $SCRIPT_PAGSEGURO; ?>"></script>
         <script src="./js/personalizado.js"></script>
     </body>
 </html>
