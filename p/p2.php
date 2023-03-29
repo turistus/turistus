2df5a57d60d629b98883e1d45918388ce9492cec
turistus <desconectaprincipal@hotmail.com>
2023-03-28 22:44:08 -0300

    Header..

--- p/p2.php
+++ p/p2.php
@@ -14,9 +14,7 @@ $idGuia = filter_input(INPUT_GET, "idGuia", FILTER_SANITIZE_NUMBER_INT);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
           <!-- Fonts and icons -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+        <link rel="shortcut icon" href="../images/icon/logo.ico" >
         <title>Turist Us - PagSeguro</title>
     <style>
         .img-band{
@@ -304,11 +302,12 @@ $idGuia = filter_input(INPUT_GET, "idGuia", FILTER_SANITIZE_NUMBER_INT);
         include_once '../rodape.php';
         ?>
 
+
+    </body>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" ></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" ></script>
 
         <script type="text/javascript" src="<?php echo SCRIPT_PAGSEGURO; ?>"></script>
         <script src="./js/personalizado.js"></script>
-    </body>
 </html>
