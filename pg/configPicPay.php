998bd775a379726b5ceb3bc57d2d59f2a2fe762a
turistus <desconectaprincipal@hotmail.com>
2023-04-02 02:12:06 -0300

    picreturn

--- pg/configPicPay.php
+++ pg/configPicPay.php
@@ -4,8 +4,8 @@ define('PICPAYTOKEN', "3d14c4a4-932e-4348-9221-f714aecfcce2");
 
 define('XSELLERTOKEN', "eeda8a78-eb85-435e-952e-0795e4e6930a");
 
-define('CALLBACKURL', "http://localhost/empresa/funcoes/situacaoPagamento.php");
-define('RETURNURL', "http://localhost/empresa/usuarios/painelUsuario.php");
+define('CALLBACKURL', "https://turistus.com.br/pagamentoEventoForm.php");
+define('RETURNURL', "https://turistus.com.br/pagamentoEventoForm.php");
 
 //define('CALLBACKURL', "http://www.sualoja.com.br/callback");
 //define('RETURNURL', "http://www.sualoja.com.br/cliente/pedido/");
