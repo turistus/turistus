7d0fe460f87b257e38dd254399779e5a552d45c5
turistus <desconectaprincipal@hotmail.com>
2023-04-04 15:09:32 -0300

    isso

--- usuarios/PainelTurista.php
+++ usuarios/PainelTurista.php
@@ -15,7 +15,7 @@ $emailUsuario = $_SESSION['user_email'];
 
 ?>
 
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt/br">
 
 <head>
@@ -183,7 +183,7 @@ $emailUsuario = $_SESSION['user_email'];
 </body>
 
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
   $(document).ready(function() {
