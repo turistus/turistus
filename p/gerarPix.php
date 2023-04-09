d77c7729e0c51018c878686d488459a3032b6ce5
turistus <desconectaprincipal@hotmail.com>
2023-04-09 19:31:13 -0300

    sem o ordens

--- p/gerarPix.php
+++ p/gerarPix.php
@@ -2,7 +2,7 @@
 <?php
 
 
-$endpoint = 'https://api.pagseguro.com/orders';
+$endpoint = 'https://api.pagseguro.com';
 $token = '2ec1526c-6da7-467f-b950-73129b6b5fc4e192b72c44e081c68c58638efbbbd16f318d-49a8-4a82-be6d-77f0366b88d2';
 $reference_id = "158";
 
@@ -50,7 +50,7 @@ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, [
   'Content-Type:application/json',
-  'Authorization: Bearer '.$token
+  'Authorization: Bearer'.$token
 ]);
 
 $response = curl_exec($curl);
