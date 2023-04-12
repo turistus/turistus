c1b25e99f7b43047b39cebd8cf48bba853f21951
turistus <desconectaprincipal@hotmail.com>
2023-04-12 18:13:17 -0300

    chamando pagdireto

--- pagarPagSeguro/Libraries/chamaPagDireto.js
+++ pagarPagSeguro/Libraries/chamaPagDireto.js
@@ -5,7 +5,7 @@ $('#BotaoPagamento').on('click',function(event){
     event.preventDefault();
 
     $.ajax({
-        url: URL+"pagarPagSeguro/PagamentoDireto.php",
+        url: URL+"/pagarPagSeguro/PagamentoDireto.php",
         type: 'POST',
         dataType:'html',
         success:function(data){
