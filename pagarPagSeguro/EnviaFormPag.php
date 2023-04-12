1029df22fa94332d36519a7b9c0a649cecf8c7b0
turistus <desconectaprincipal@hotmail.com>
2023-04-12 18:29:22 -0300

    nunca que ia abrir

--- pagarPagSeguro/EnviaFormPag.php
+++ pagarPagSeguro/EnviaFormPag.php
@@ -10,7 +10,7 @@
 </head>
 <body>
 
-    <form name="FormPagamento" id="FormPagamento" action="https://sandbox.pagseguro.uol.com.br/v2/checkout/payment.html?action=add" method="post">
+    <form name="FormPagamento" id="FormPagamento" action="https://sandbox.pagseguro.uol.com.br/checkout/v2/cart.html?action=add" method="post">
         <!-- N�?O EDITE OS COMANDOS DAS LINHAS ABAIXO -->
         <input type="text" name="code" id="code" value="" />
         <input type="hidden" name="iot" value="button" />
