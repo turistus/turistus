b72da89866915eef94d606373bd17c9a9fb9021e
turistus <desconectaprincipal@hotmail.com>
2023-04-12 01:44:57 -0300

    ae deu posso voltar ao include

--- index.php
+++ index.php
@@ -301,80 +301,12 @@ ob_start();
       </div>
     </div>
     <!-- FINAL ONDE BUSCA OS PONTOS TURISTICOS -->
-    <footer style="position: absolute;
 
-      background-image: url('./images/fundofooter.jpg');
-      background-size: cover;">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-3">
-        <h3>Contato</h3>
-        <p>Endereço: Rua das Flores, 123</p>
-        <p>Telefone: (11) 5555-5555</p>
-        <p>E-mail: contato@turistus.com.br</p>
-        <div class="social-icons">
-          <a href="#" class="fa fa-facebook"></a>
-          <a href="#" class="fa fa-twitter"></a>
-          <a href="#" class="fa fa-instagram"></a>
-        </div>
-      </div>
-      <div class="col-md-3">
-        <h3>Navegação</h3>
-        <ul class="list-unstyled">
-          <li><a href="#">Pacotes de viagem</a></li>
-          <li><a href="#">Destinos</a></li>
-          <li><a href="#">Promoções</a></li>
-          <li><a href="#">Sobre nós</a></li>
-          <li><a href="#">Contato</a></li>
-        </ul>
-      </div>
-      <div class="col-md-3">
-        <h3>Idioma e Moeda</h3>
-        <select class="form-control">
-          <option value="pt-br">Português</option>
-          <option value="en">English</option>
-          <option value="es">Español</option>
-        </select>
-        <select class="form-control">
-          <option value="BRL">Real (R$)</option>
-          <option value="USD">Dólar ($)</option>
-          <option value="EUR">Euro (€)</option>
-        </select>
-      </div>
-      <div class="col-md-3">
-        <h3>Newsletter</h3>
-        <p>Inscreva-se em nossa newsletter para receber as últimas notícias e promoções.</p>
-        <form action="#">
-          <div class="form-group">
-            <input type="email" class="form-control" placeholder="Seu e-mail">
-          </div>
-          <button type="submit" class="btn btn-primary">Inscrever-se</button>
-        </form>
-      </div>
-    </div>
-    <hr>
-    <div class="row">
-      <div class="col-md-6">
-        <p>&copy; 2023 Turistus. Todos os direitos reservados.</p>
-      </div>
-      <div class="col-md-6">
-        <ul class="list-inline text-right">
-          <li><a href="#">Termos de uso</a></li>
-          <li><a href="#">Política de privacidade</a></li>
-          <li><a href="#">Selo de segurança</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-
-</footer>
 
   </main><!-- /.container -->
-
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
+  <?php
+  include_once 'rodape.php';
+  ?>
 
 </body>
 
