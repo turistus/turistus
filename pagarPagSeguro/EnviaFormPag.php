681e73b8ce194beffce9637d9a7c4a275eeeb17e
turistus <desconectaprincipal@hotmail.com>
2023-04-12 23:49:49 -0300

    caminhos só pra ve

--- pagarPagSeguro/EnviaFormPag.php
+++ pagarPagSeguro/EnviaFormPag.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>API Pagseguro</title>
 </head>
 <body>
@@ -17,8 +17,8 @@
         <input id="BotaoPagamento" type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/pagamentos/209x48-pagar-azul-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
     </form>
 
-    <script src="Libraries/zepto.min.js"></script>
-    <script src="Libraries/chamaPagDireto.js"></script>
+    <script src="pagarPagSeguro/Libraries/zepto.min.js"></script>
+    <script src="pagarPagSeguro/Libraries/chamaPagDireto.js"></script>
 
 </body>
 </html>
