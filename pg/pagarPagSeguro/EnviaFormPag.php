0a43618399458bc8fa8f34bf75e880e3fea1a05c
turistus <desconectaprincipal@hotmail.com>
2023-04-14 22:18:09 -0300

    teste

--- pg/pagarPagSeguro/EnviaFormPag.php
+++ pg/pagarPagSeguro/EnviaFormPag.php
@@ -42,8 +42,7 @@ include_once '../../connection.php';
 <html lang="pt-br">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css">
+
         <link rel="shortcut icon" href="../../images/logooriginal.png" >
         <title>Turist Us - Formulario de Pagamento</title>
     </head>
