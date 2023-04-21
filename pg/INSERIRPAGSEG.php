710ed8d127d02386a0c3211990bc1751e5914b98
turistus <desconectaprincipal@hotmail.com>
2023-04-20 23:53:59 -0300

    teste insert

--- pg/INSERIRPAGSEG.php
+++ pg/INSERIRPAGSEG.php
@@ -7,23 +7,20 @@
         <title>TESTE INSERIR</title>
     </head>
     <body>
+        <h1> Testando um insert </h1>
 
 
 <?php
 define('ACCESS', true);
 ob_start();
 include("../connection.php");
-//Receber os dados do formulário
-$descreveEvento = filter_input_array(INPUT_POST, FILTER_DEFAULT);
+
 
 $titulo = "ALOHA";
 $idEv = 0545;
 $descricao ="ALoha é Familia";
 $custoEvento = 10.80;
 $idGuia = 01;
-
-// A variável recebe a mensagem de erro
-$msg = "";
 
 // Acessar o IF quando o usuário clica no botão
 if (isset($titulo)){
@@ -48,17 +45,6 @@ echo "ok CHEGOU AQUI DPS DO INSERT";
 
 ?>
 
-
-<form name="Formuo" id="Formuo" action="INSERIRPAGSEG.php" method="POST">
-    <!-- N�?O EDITE OS COMANDOS DAS LINHAS ABAIXO -->
-    <input type="hidden" name="titulo" id="titulo" value="SORTE" />
-    <input type="hidden" name="idEv" id="idEv" value="33" />
-    <input type="hidden" name="custoEvento" id="custoEvento" value="13.50" />
-    <input type="hidden" name="descricao" id="descricao" value="A Lagoa Perdida" />
-    <input type="hidden" name="idGuia" id="idGuia" value="01" />
-
-    <input id="BtnPagS" type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/pagamentos/209x48-pagar-azul-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
-</form>
 
 
 
