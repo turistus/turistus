8dcc915fa833e7b624128bc3675e0b7aa99698dd
turistus <desconectaprincipal@hotmail.com>
2023-04-21 00:16:51 -0300

    caminho

---
+++ INSERIRPAGSEG.php
@@ -0,0 +1,21 @@
+
+<?php
+define('ACCESS', true);
+
+include("../connection.php");
+
+
+$titulo = "ALOHA";
+$idEv = 0545;
+$descricao ="ALoha é Familia";
+$custoEvento = 10.80;
+$idGuia = 01;
+
+
+$query_pa = "INSERT INTO payments_pagSeg (titulo, idEv, descricao, custoEvento, idGuia, dataGerada) VALUES ($titulo, $idEv, $descricao, $custoEvento, $idGuia, '0000-00-00')";
+$add_pagSeg = $conn->prepare($query_pa);
+$add_pagSeg->execute();
+// FIM DA INSERT EM PAYMENTS PICPAY
+echo "ok CHEGOU AQUI DPS DO INSERT";
+
+?>
