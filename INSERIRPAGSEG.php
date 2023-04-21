905f77a7f388fb8a67033a6eefe5887d80c0ea94
turistus <desconectaprincipal@hotmail.com>
2023-04-21 00:18:58 -0300

    gogo

--- INSERIRPAGSEG.php
+++ INSERIRPAGSEG.php
@@ -4,18 +4,10 @@ define('ACCESS', true);
 
 include("../connection.php");
 
-
-$titulo = "ALOHA";
-$idEv = 0545;
-$descricao ="ALoha é Familia";
-$custoEvento = 10.80;
-$idGuia = 01;
-
-
-$query_pa = "INSERT INTO payments_pagSeg (titulo, idEv, descricao, custoEvento, idGuia, dataGerada) VALUES ($titulo, $idEv, $descricao, $custoEvento, $idGuia, '0000-00-00')";
+$query_pa = "INSERT INTO payments_pagSeg (titulo, idEv, descricao, custoEvento, idGuia, dataGerada) VALUES ('ALOHA', 0545, 'ALoha é Familia', 10.80, 1, '0000-00-00')";
 $add_pagSeg = $conn->prepare($query_pa);
 $add_pagSeg->execute();
 // FIM DA INSERT EM PAYMENTS PICPAY
-echo "ok CHEGOU AQUI DPS DO INSERT";
+echo "<h1> ok CHEGOU AQUI DPS DO INSERT </h1>";
 
 ?>
