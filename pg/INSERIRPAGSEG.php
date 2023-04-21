8b659c1e58bede4319164a6e0d434e8fa286a764
turistus <desconectaprincipal@hotmail.com>
2023-04-20 23:25:28 -0300

    so pra ve

--- pg/INSERIRPAGSEG.php
+++ pg/INSERIRPAGSEG.php
@@ -1,6 +1,16 @@
 
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="shortcut icon" href="images/icon/favicon.ico" >
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        <title>TESTE INSERIR</title>
+    </head>
+    <body>
 
-<form name="FormPagSeguro" id="FormPagSeguro" action="pagarPagSeguro/EnviarFormPag.php" method="POST">
+<form name="FormPagSeguro" id="FormPagSeguro" action="INSERIRPAGSEG.php" method="POST">
     <!-- N�?O EDITE OS COMANDOS DAS LINHAS ABAIXO -->
     <input type="hidden" name="titulo" id="titulo" value="SORTE" />
     <input type="hidden" name="idEv" id="idEv" value="33" />
@@ -10,6 +20,9 @@
 
     <input id="BtnPagS" type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/pagamentos/209x48-pagar-azul-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
 </form>
+
+    </body>
+</html>
 
 <?php
 
