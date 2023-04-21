be1cba9e939e7bc2df483c540dc156a508f0cc38
turistus <desconectaprincipal@hotmail.com>
2023-04-21 00:13:30 -0300

    vai nao

--- pg/INSERIRPAGSEG.php
+++ pg/INSERIRPAGSEG.php
@@ -24,15 +24,8 @@ $idGuia = 01;
 
 
 $query_pa = "INSERT INTO payments_pagSeg (titulo, idEv, descricao, custoEvento, idGuia, dataGerada)
-                                VALUES (:titulo, :idEv, :descricao, :custoEvento, :idGuia, :dataGerada)";
+                                VALUES ($titulo, $idEv, $descricao, $custoEvento, $idGuia, '0000-00-00')";
 $add_pagSeg = $conn->prepare($query_pa);
-$add_pagSeg->bindParam(":titulo", $titulo, PDO::PARAM_STR);
-$add_pagSeg->bindParam(":idEv", $idEv);
-$add_pagSeg->bindParam(":descricao", $descricao, PDO::PARAM_STR);
-$add_pagSeg->bindParam(":custoEvento", $custoEvento);
-$add_pagSeg->bindParam(":idGuia", $idGuia);
-$add_pagSeg->bindParam(":dataGerada", "0000-00-00");
-
 $add_pagSeg->execute();
 // FIM DA INSERT EM PAYMENTS PICPAY
 echo "ok CHEGOU AQUI DPS DO INSERT";
