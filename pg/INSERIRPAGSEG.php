8d6eb41b1a92b74d07da8c7f9fc1ad9325aae86b
turistus <desconectaprincipal@hotmail.com>
2023-04-21 00:09:06 -0300

    so inserir

--- pg/INSERIRPAGSEG.php
+++ pg/INSERIRPAGSEG.php
@@ -22,11 +22,9 @@ $descricao ="ALoha é Familia";
 $custoEvento = 10.80;
 $idGuia = 01;
 
-// Acessar o IF quando o usuário clica no botão
-if (isset($titulo)){
-//Salvar os dados da compra no banco de dados
+
 $query_pa = "INSERT INTO payments_pagSeg (titulo, idEv, descricao, custoEvento, idGuia, dataGerada)
-VALUES (:titulo, :idEv, :descricao, :custoEvento, :idGuia, :dataGerada)";
+                                VALUES (:titulo, :idEv, :descricao, :custoEvento, :idGuia, :dataGerada)";
 $add_pagSeg = $conn->prepare($query_pa);
 $add_pagSeg->bindParam(":titulo", $titulo, PDO::PARAM_STR);
 $add_pagSeg->bindParam(":idEv", $idEv);
@@ -38,10 +36,6 @@ $add_pagSeg->bindParam(":dataGerada", "0000-00-00");
 $add_pagSeg->execute();
 // FIM DA INSERT EM PAYMENTS PICPAY
 echo "ok CHEGOU AQUI DPS DO INSERT";
-
- }else{
-    header("Location: ./view-eventos.php");
- }
 
 ?>
 
