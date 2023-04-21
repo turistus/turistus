a9a636b32fcd6d0a9b1467bab0d2123ebdf8a237
turistus <desconectaprincipal@hotmail.com>
2023-04-21 01:48:16 -0300

    Valida carai

--- adm/validate.php
+++ adm/validate.php
@@ -8,8 +8,15 @@ if(!defined('ACCESS')){
 
 //verifica se usuario esta online
 if((($_SESSION['user_id']) == 2)){
-    unset($_SESSION['user_id'], $_SESSION['user_name'], $_SESSION['user_email'], $_SESSION['user_key']);
+
+    echo "OK ID DO ADM";
+}else{
+    unset($_SESSION['user_id'],
+    $_SESSION['user_name'],
+    $_SESSION['user_email'],
+    $_SESSION['user_key']);
     $_SESSION['msg'] = "<div class='alert alert-danger' role='alert'>Erro: Necessário realizar o login para acessar a página!</div>";
     header("Location: index.php");
     die("Erro: Página não encontrada!<br>");
+
 }
