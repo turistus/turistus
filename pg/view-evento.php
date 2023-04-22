47370e475e8deb492af78caab55774f34e97d7af
turistus <desconectaprincipal@hotmail.com>
2023-04-21 23:32:15 -0300

    sera

--- pg/view-evento.php
+++ pg/view-evento.php
@@ -161,11 +161,11 @@ session_start();
 
 
                                             <form name="FormPagSeg" id="FormPagSeg" action="../inserirPs.php" method="POST">
-                                                <input type="hidden" name="titulo" id="titulo" value="Aloha" />
-                                                <input type="hidden" name="idEv" id="idEv" value="87" />
+                                                <input type="hidden" name="titulo" id="titulo" value="Alo Brasil" />
+                                                <input type="hidden" name="idEv" id="idEv" value="93" />
                                                 <input type="hidden" name="descricao" id="descricao" value="Aloha é familia" />
-                                                <input type="hidden" name="custoEvento" id="custoEvento" value="10" />
-                                                <input type="hidden" name="idGuia" id="idGuia" value="01" />
+                                                <input type="hidden" name="custoEvento" id="custoEvento" value="99" />
+                                                <input type="hidden" name="idGuia" id="idGuia" value="02" />
                                                 <input id="BotaoPagar" type="image" src="https://stc.pagseguro.uol.com.br/public/img/botoes/pagamentos/209x48-pagar-azul-assina.gif" name="submit" alt="Pague com PagSeguro - é rápido, grátis e seguro!" />
                                             </form>
 
