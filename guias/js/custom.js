d2097e1d0d0284a3978ae1ba21f044ac824fc3fc
turistus <desconectaprincipal@hotmail.com>
2023-04-27 18:24:52 -0300

    aline

--- guias/js/custom.js
+++ guias/js/custom.js
@@ -30,33 +30,33 @@ function adicionarCampo(){
     var controleCampo = 1;
     controleCampo++;
     document.getElementById('formulario').insertAdjacentHTML('beforeend',''+
-    '<div class="form-group" id="bloco' + controleCampo +'" style="border: 1px solid green; padding:15px;">'+
-    '<button type="button" id="'+ controleCampo +'" onclick="removerCampo('+ controleCampo +')"> - </button><br>'+
-    '<label>  N° Vagas  </label>'+
-        '<select id="vagas" name="vagas[]" class="custom-select col-3 " required>'+
-                '<option value="">Selecione</option>'+
-                '<option value="01">01 Pessoa</option>'+
-                '<option value="02">02 Pessoas</option>'+
-                '<option value="03">03 Pessoas</option>'+
-                '<option value="04">04 Pessoas</option>'+
-                '<option value="05">05 Pessoas</option>'+
-                '<option value="06">06 Pessoas</option>'+
-                '<option value="07">07 Pessoas</option>'+
-                '<option value="08">08 Pessoas</option>'+
-                '<option value="09">09 Pessoas</option>'+
-                '<option value="10">10 Pessoas</option>'+
-                '<option value="11">11 Pessoas</option>'+
-                '<option value="12">12 Pessoas</option>'+
-                '<option value="13">13 Pessoas</option>'+
-                '<option value="14">14 Pessoas</option>'+
-                '<option value="15">15 Pessoas</option>'+
-                '<option value="16">16 Pessoas</option>'+
-                '<option value="17">17 Pessoas</option>'+
-                '<option value="18">18 Pessoas</option>'+
-                '<option value="19">19 Pessoas</option>'+
-                '<option value="20">20 Pessoas</option>'+
-        '</select>'+
-    '<label> Total R$ </label>'+
+    '<div class="form-group form-inline" id="bloco' + controleCampo +'" style="border: 1px solid green; padding:15px;">'+
+        '<button type="button" id="'+ controleCampo +'" onclick="removerCampo('+ controleCampo +')"> - </button><br>'+
+        '<label>  N° Vagas  </label> '+
+            '<select id="vagas" name="vagas[]" class="custom-select col-3 " required>'+
+                    '<option value="">Selecione</option>'+
+                    '<option value="01">01 Pessoa</option>'+
+                    '<option value="02">02 Pessoas</option>'+
+                    '<option value="03">03 Pessoas</option>'+
+                    '<option value="04">04 Pessoas</option>'+
+                    '<option value="05">05 Pessoas</option>'+
+                    '<option value="06">06 Pessoas</option>'+
+                    '<option value="07">07 Pessoas</option>'+
+                    '<option value="08">08 Pessoas</option>'+
+                    '<option value="09">09 Pessoas</option>'+
+                    '<option value="10">10 Pessoas</option>'+
+                    '<option value="11">11 Pessoas</option>'+
+                    '<option value="12">12 Pessoas</option>'+
+                    '<option value="13">13 Pessoas</option>'+
+                    '<option value="14">14 Pessoas</option>'+
+                    '<option value="15">15 Pessoas</option>'+
+                    '<option value="16">16 Pessoas</option>'+
+                    '<option value="17">17 Pessoas</option>'+
+                    '<option value="18">18 Pessoas</option>'+
+                    '<option value="19">19 Pessoas</option>'+
+                    '<option value="20">20 Pessoas</option>'+
+            '</select>'+
+    ' <label> Total R$ </label>'+
         '<input class="form-control col-3" type="text"  name="total[]" id="total" value="" required> <br>'+
     '</div>');
 }
