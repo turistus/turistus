0a1cf9c6757bcdf3b4e797602d0987769ed30780
turistus <desconectaprincipal@hotmail.com>
2023-04-27 22:12:55 -0300

    kkk

--- guias/funcoes/buscaEventos.php
+++ guias/funcoes/buscaEventos.php
@@ -77,6 +77,8 @@ $Uid = $_SESSION['user_id'];
                           ?>
                       </div>
 
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
