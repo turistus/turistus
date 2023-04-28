47d537258d28f5301a94ad594cacdbf53d94655b
turistus <desconectaprincipal@hotmail.com>
2023-04-28 17:24:11 -0300

    a

--- guias/painelGuia.php
+++ guias/painelGuia.php
@@ -337,7 +337,7 @@ $emailusuario = $_SESSION['user_email'];
                                 <div class="modal-content" id=formQuarto style="padding-bottom: 20px;">
 
                                   <?php
-                                      include_once '../guias/buscaEventos.php';
+                                      include_once './guias/buscaEventos.php';
                                   ?>
 
                                 </div>
