460ff9b70df06fa0ee1a37747f6047d70f5f9f19
turistus <desconectaprincipal@hotmail.com>
2023-04-28 17:02:30 -0300

    so pra ve ce é o js merda

--- guias/painelGuia.php
+++ guias/painelGuia.php
@@ -337,7 +337,7 @@ $emailusuario = $_SESSION['user_email'];
                                 <div class="modal-content" id=formQuarto style="padding-bottom: 20px;">
 
                                   <?php
-                                     // include_once 'funcoes/buscaEventos.php';
+                                      include_once 'funcoes/listaEventosPerfilGuia.php';
                                   ?>
 
                                 </div>
@@ -366,7 +366,7 @@ $emailusuario = $_SESSION['user_email'];
 
 </body>
 </html>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script>
 
