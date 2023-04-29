f03c12c4cc6131549fcbffde0cfed5fcd151eb41
turistus <desconectaprincipal@hotmail.com>
2023-04-29 19:11:12 -0300

    col-6

--- guias/testeVisualizarValores.php
+++ guias/testeVisualizarValores.php
@@ -5,11 +5,11 @@ include_once '../connection.php';
 //$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 $id = 96;
 ?>
+<div class="row">
 
+    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-6" >
 
-<div class="col-6 " >
-
-                                <label for="vagas">  N° Vagas  </label>
+                                <label for="total">  N° Vagas  </label>
 
                                     <?php
                                     $buscaValores = "SELECT valores.id AS idVal, idEvento, vagas, total FROM valores WHERE idEvento = $id ORDER BY idVal";
@@ -21,7 +21,7 @@ $id = 96;
 
                                     ?>
                                     <!-- Check box do VALOR TOTAL SELECIONADO na Compra -->
-                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6" style="padding: 10px; border: 1px solid black;">
+                                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6" style="padding: 10px;">
                                         <div class="col-auto my-1">
                                             <div class="custom-control custom-checkbox mr-sm-2">
                                                 <input type="checkbox" class="custom-control-input" id="total" name="total" value="">
@@ -29,11 +29,6 @@ $id = 96;
                                             </div>
                                         </div>
                                     </div>
-                                    <br>
-
-
-
-                                        <hr>
 
                                     <?php
                                         }
@@ -41,3 +36,5 @@ $id = 96;
                                     ?>
                                     <br>
                             </div>
+
+</div>
