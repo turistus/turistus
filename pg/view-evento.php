7b8a44111f482003c1fea741647f6b43f135c1a7
turistus <desconectaprincipal@hotmail.com>
2023-04-28 21:08:05 -0300

    pessoa

--- pg/view-evento.php
+++ pg/view-evento.php
@@ -150,10 +150,10 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
                                                     <?php
                                                         foreach($res as $ln ){
                                                     ?>
-                                                        <option value="<?php echo $ln['vagas']['0'];?>" name="vagas" id="vagas" >
+                                                        <option value="<?php echo $ln['vagas']['0'];?>" name="vagas" id="vagas" >Pessoa
                                                     <?php
                                                         // echo $ln['id'].' - <br/>';
-                                                        echo $ln['vagas']['0'].' - '.$ln['valor']['0'];
+                                                        //echo $ln['vagas']['0'].' - '.$ln['valor']['0'];
                                                     }
                                                     ?>
                                                         </option>
