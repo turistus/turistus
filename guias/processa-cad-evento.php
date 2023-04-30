ff3034c51daa291a66c97e896025c6c471de6e46
turistus <desconectaprincipal@hotmail.com>
2023-04-30 01:02:22 -0300

    direto

--- guias/processa-cad-evento.php
+++ guias/processa-cad-evento.php
@@ -4,19 +4,21 @@ include_once '../connection.php';
 
 //Receber os dados do formulário
 $dados_CadEvento = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-var_dump($dados_CadEvento['*']."VAAAR");
+var_dump($dados_CadEvento."VAAAR");
 echo $dados_CadEvento;
 $novaData = date("Y/m/d");
 echo $novaData;
 
 if(!empty($dados_CadEvento['Cadastrar'])){
         $last_id = 95;
+        $v1=1;
+        $v2=10;
 
         $CriarValores = "INSERT INTO valores (idEvento, vagas, total ) VALUES (:idEvento, :vagas, :total) ";
         $preparandoQuerySQL = $conn->prepare($CriarValores);
         $preparandoQuerySQL->bindParam(':idEvento', $last_id);
-        $preparandoQuerySQL->bindParam(':vagas',1);
-        $preparandoQuerySQL->bindParam(':total', 20);
+        $preparandoQuerySQL->bindParam(':vagas',$v1);
+        $preparandoQuerySQL->bindParam(':total', $v2);
         $preparandoQuerySQL->execute();
 
 }
