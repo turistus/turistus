ff2f3d0a7172742ab2b75ae38bc88767386fd3a6
turistus <desconectaprincipal@hotmail.com>
2023-05-03 01:41:10 -0300

    depende tamanho no js

--- pg/view-evento.php
+++ pg/view-evento.php
@@ -373,10 +373,11 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
 
     $(document).ready(function(){
     $('.carousel').slick({
-      slidesToShow: 3,
+      slidesToShow: 1,
       slidesToScroll: 1,
       autoplay: true,
       autoplaySpeed: 2000,
+      fade: true,
       arrows: true,
       prevArrow: '<button type="button" class="slick-prev">Previous</button>',
       nextArrow: '<button type="button" class="slick-next">Next</button>',
@@ -394,6 +395,7 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
           }
         }
       ]
+
     });
   });
 
