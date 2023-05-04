7d5f759d763bb0a590c595e67f73bc39b9f0cb4b
turistus <desconectaprincipal@hotmail.com>
2023-05-04 04:39:26 -0300

    ae

--- pg/funcoes/separarListaValores.php
+++ pg/funcoes/separarListaValores.php
@@ -1,13 +1,13 @@
 <?php
 // verifica cada campo booleano individualmente e exibe apenas os que são verdadeiros
 if ($alimentacao == 1) {
-    echo "<li style='margin-right: 22px;'>";
+    echo "<li style='margin-right: 22px; display:none;'>";
     echo " Refeição: <i class='fa-solid fa-burger'></i> ";
     echo "</li>";
 
   }
   if ($transporte == 1) {
-    echo "<li>";
+    echo "<li style=' display:none;'>";
     echo " Transporte: <i class='fa-solid fa-car'></i> ";
     echo "</li>";
 
