f3de2dab9e24c48a981f94f4c7033952c04fc21b
turistus <desconectaprincipal@hotmail.com>
2023-05-06 02:26:23 -0300

    a

--- guias/funcoes/delEvento.php
+++ guias/funcoes/delEvento.php
@@ -1,9 +1,9 @@
 <?php
 
 define('ACCESS', true);
-include_once '../connection.php';
+include_once '../../connection.php';
 session_start();
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_POST, "id", FILTER_SANITIZE_NUMBER_INT);
 
 //Editar a compra informado o status da compra no PicPay
 $query_deletar_evento = "DELETE FROM eventos WHERE eventos.id = $id";
