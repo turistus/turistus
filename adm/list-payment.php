e49f1c39b1d8befb8fd187b2ef620f70b97b0949
turistus <desconectaprincipal@hotmail.com>
2023-05-07 00:31:49 -0300

    posicao

--- adm/list-payment.php
+++ adm/list-payment.php
@@ -39,18 +39,21 @@ include_once '../adm/validate.php';
 
             <table class="table table-bordered table-striped table-hover">
                 <thead>
-                    <tr style="font-size: 1.5em;">
+                    <tr style="font-size: 0.8em;">
+                        <th scope="col" class="text-center">Status</th>
+                        <th scope="col" class="text-center">Ações</th>
+
                         <th scope="col">ID</th>
                         <th scope="col">Data Pedido</th>
+                        <th scope="col">Data Agendada</th>
                         <th scope="col">Turista</th>
                         <th scope="col">E-mail </th>
                         <th scope="col">Evento</th>
                         <th scope="col">Vagas</th>
                         <th scope="col">Total</th>
 
-                        <th scope="col" class="text-center">Status</th>
-                        <th scope="col" class="text-center">Ações</th>
                         <th scope="col" class="text-center">Guia Nativo</th>
+
                     </tr>
                 </thead>
                 <?php
@@ -60,6 +63,7 @@ include_once '../adm/validate.php';
                     pay.first_name,
                     pay.email,
                     pay.created,
+                    pay.dataagendada,
 
                     prod.name AS name_prod,
 
@@ -78,8 +82,6 @@ include_once '../adm/validate.php';
                     INNER JOIN turistas ON turistas.email = pay.email
                     INNER JOIN valores ON valores.idEventoVal = pay.product_id
                     INNER JOIN eventos ON eventos.idEvento = valores.idEventoVal
-
-
                     INNER JOIN payments_status AS sta ON sta.id=pay.payments_statu_Id
                     ORDER BY pay.id DESC ";
                 $result_payments = $conn->prepare($query_payments);
@@ -87,20 +89,23 @@ include_once '../adm/validate.php';
                 while ($row_payment = $result_payments->fetch(PDO::FETCH_ASSOC)) {
                     //var_dump($row_payment);
                     extract($row_payment);
+                    $totalFormatado = number_format($total, 2, ",", ".");
                     echo "<tr>";
-                    echo "<th>$id</th>";
-                    echo "<th>". date('d/m/Y',  strtotime($created)) ."</th>";
-                    echo "<td>$first_name</td>";
-                    echo "<td>$email</td>";
-                    echo "<td>$nomeE</td>";
-                    echo "<td>$vagas</td>";
-                    echo "<td>$total</td>";
-                    echo "<td class='text-center'><span class='badge badge-pill badge-$color'>$name_sta</span></td>";
-                    echo "<td class='text-center'>";
-                    echo "<a href='payment-status.php?id=$id' class='btn btn-outline-primary btn-sm'>Status</a> ";
-                    echo "<a href='cancel-payment.php?id=$id' class='btn btn-outline-danger btn-sm'>Cancelar</a>";
-                    echo "</td>";
-                    echo "<td>ID GUIA nome</td>";
+                        echo "<td class='text-center'><span class='badge badge-pill badge-$color'>$name_sta</span></td>";
+                        echo "<td class='text-center'>";
+                            echo "<a href='payment-status.php?id=$id' class='btn btn-outline-primary btn-sm'>Status</a> ";
+                            echo "<a href='cancel-payment.php?id=$id' class='btn btn-outline-danger btn-sm'>Cancelar</a>";
+                        echo "</td>";
+
+                        echo "<th>$id</th>";
+                        echo "<th>". date('d/m/Y',  strtotime($created)) ."</th>";
+                        echo "<th>". date('d/m/Y',  strtotime($dataagendada)) ."</th>";
+                        echo "<td>$first_name</td>";
+                        echo "<td>$email</td>";
+                        echo "<td>$nomeE</td>";
+                        echo "<td>$vagas</td>";
+                        echo "<td>$totalFormatado</td>";
+                        echo "<td>ID GUIA nome</td>";
                     echo "</tr>";
 
                 }
