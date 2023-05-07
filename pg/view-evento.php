668fe88f67282365b8316e8e0df30d20a141c33d
turistus <desconectaprincipal@hotmail.com>
2023-05-07 17:07:50 -0300

    sera q pega

--- pg/view-evento.php
+++ pg/view-evento.php
@@ -225,7 +225,7 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
                                             <input type="hidden" name="id" id="id" value="<?php echo $id;?>" />
                                             <input type="hidden" name="idGuia" id="idGuia" value="<?php echo $idGuia;?>" />
                                             <input type="hidden" name="email" id="email" value="<?php echo $emailSessaoAberta;?>" />
-                                            <input type="hidden" name="idVal" id="idVal" value="<?php echo $idVal;?>" />
+                                            <option value="<?php echo $ln['idVal'];?>" name="idVal" id="idVal" >
                                             <input type="hidden" name="encontro" id="encontro" value="<?php echo $encontro;?>" />
 
                                             </form>
