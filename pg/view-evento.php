f00e77e41fd1f7b3bc74675d69b00f0b2c7da7ed
turistus <desconectaprincipal@hotmail.com>
2023-05-10 05:21:20 -0300

    ave

--- pg/view-evento.php
+++ pg/view-evento.php
@@ -216,7 +216,7 @@ $id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
                                                 <input type="hidden" name="id" id="id" value="<?php echo $id;?>" />
                                                 <input type="hidden" name="idGuia" id="idGuia" value="<?php echo $idGuia;?>" />
                                                 <input type="hidden" name="email" id="email" value="<?php echo $emailSessaoAberta;?>" />
-                                                <input type="hidden" name="opcaoSelecionada" id="opcaoSelecionada" value="<?php echo $ln['idVal'];?>"/>
+                                                <input type="hidden" name="opcaoSelecionada" id="opcaoSelecionada" value=""/>
                                                 <input type="hidden" name="encontro" id="encontro" value="<?php echo $encontro;?>" />
 
 
