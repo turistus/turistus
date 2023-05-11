018c425062b2be6416a23b62a915e7a232716624
turistus <desconectaprincipal@hotmail.com>
2023-05-11 04:25:27 -0300

    a

--- pg/pagamentoEventoForm.php
+++ pg/pagamentoEventoForm.php
@@ -99,7 +99,7 @@ include_once './configPicPay.php';
         echo "VARIAVEL DATA: ". $data;
 
         // Acessar o IF quando o usuário clica no botão
-        if (isset($data['BtnPicPay'])) {
+        if (!isset($data['BtnPicPay'])) {
             echo "VARIAVEL DATA dentro do IF: ". $data;
             var_dump($data);
             $empty_input = false;
