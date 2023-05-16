96c73f6379670e87b2bcb952683d7029f09ecfe7
turistus <desconectaprincipal@hotmail.com>
2023-05-16 02:40:29 -0300

    a

--- pg/pagamentoEventoForm.php
+++ pg/pagamentoEventoForm.php
@@ -71,7 +71,7 @@ echo " encontro:".$dados_pagamento['encontro']." ";
         FROM eventos
 
         INNER JOIN servicos AS svcs ON svcs.id=eventos.idGuia
-        LEFT JOIN valores AS val ON val.idEvento=eventos.id GROUP BY eventos.id
+        LEFT JOIN valores AS val ON val.idEvento=eventos.id
         WHERE eventos.id =:id LIMIT 1 ";
 
         $result_products = $conn->prepare($query_products);
