6f641c2593648282957365e76c9b402cddb9d346
turistus <desconectaprincipal@hotmail.com>
2023-05-30 02:23:44 -0300

    aaa

--- usuarios/funcoes/etapasEvento.php
+++ usuarios/funcoes/etapasEvento.php
@@ -15,7 +15,26 @@ if(($pedido_selecionado) AND ($pedido_selecionado->rowCount() != 0) ){
         <h6>Pedido #<?php echo $row_Pedido['id']; ?>
         Data Agendada: <?php echo date('d/m/Y',  strtotime($row_Pedido['dataagendada'])); ?>
 
-        Etapa #<?php echo $row_Pedido['payments_statu_Id']; ?>
+        Etapa #<?php if($row_Pedido['payments_statu_Id'] == 1){
+            echo "Pague o evento";
+         } elseif($row_Pedido['payments_statu_Id'] == 2){
+            echo "Pague o evento";
+         }
+         elseif($row_Pedido['payments_statu_Id'] == 3){
+            echo "Cancelado";
+         }
+         elseif($row_Pedido['payments_statu_Id'] == 4){
+            echo "Pagamento em Analise !";
+         }
+         elseif($row_Pedido['payments_statu_Id'] == 5){
+            echo "Pago !";
+         }
+         elseif($row_Pedido['payments_statu_Id'] == 6){
+            echo "Pago, Aguarde confirmaçao do guia";
+         }
+         elseif($row_Pedido['payments_statu_Id'] == 7){
+            echo "Estornado o pagamento";
+         }?>
         <?php if($row_Pedido['confirmado'] == 1){echo "<p style='background:green;'>Evento Confirmado </p>"; }else{ echo "Aguarde o guia aceitar !"; }; ?></h6>
         <hr>
 
