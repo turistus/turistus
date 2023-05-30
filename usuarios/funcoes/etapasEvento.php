38e4bba6e37be9ef2dfdad83eab2dc31a3dbc6ab
turistus <desconectaprincipal@hotmail.com>
2023-05-30 01:42:13 -0300

    aaaaa

--- usuarios/funcoes/etapasEvento.php
+++ usuarios/funcoes/etapasEvento.php
@@ -3,7 +3,7 @@ echo "Email".$emailUsuario;
 
 
 
-$query_busca_Pedido = "SELECT *, dataagendada, payments_statu_Id FROM payments_picpays WHERE payments_picpays.email = $emailUsuario LIMIT 1" ;
+$query_busca_Pedido = "SELECT *, dataagendada, payments_statu_Id FROM payments_picpays WHERE payments_picpays.email = $emailUsuario" ;
 $pedido_selecionado = $conn->prepare($query_busca_Pedido);
 $pedido_selecionado->execute();
 
@@ -11,9 +11,8 @@ echo  "etapaaa: " . $payments_statu_Id;
 if(($pedido_selecionado) AND ($pedido_selecionado->rowCount() != 0) ){
     $row_Pedido = $pedido_selecionado->fetch(PDO::FETCH_ASSOC);
 
-        if($row_Pedido['payments_statu_Id'] === 5){
             echo "Eeeeetapa: ".$row_Pedido['payments_statu_Id'];
-        }
+
 
     }else {
         header("Location: ../index.php");
