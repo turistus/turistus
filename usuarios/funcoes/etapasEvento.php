f2f32ff390b693563ac36c8d84988fd71910a0fd
turistus <desconectaprincipal@hotmail.com>
2023-05-30 01:39:16 -0300

    a

--- usuarios/funcoes/etapasEvento.php
+++ usuarios/funcoes/etapasEvento.php
@@ -8,8 +8,8 @@ $pedido_selecionado = $conn->prepare($query_busca_Pedido);
 $pedido_selecionado->execute();
 
 echo  "etapaaa: " . $payments_statu_Id;
-
-    if($row_Pedido = $pedido_selecionado->fetch(PDO::FETCH_ASSOC)){
+if(($pedido_selecionado) AND ($pedido_selecionado->rowCount() != 0) ){
+    $row_Pedido = $pedido_selecionado->fetch(PDO::FETCH_ASSOC);
 
         if($row_Pedido['payments_statu_Id'] === 5){
             echo "Eeeeetapa: ".$row_Pedido['payments_statu_Id'];
