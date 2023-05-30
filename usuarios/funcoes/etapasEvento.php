0131160513bef17b1c83338f3694c3b89ae020b6
turistus <desconectaprincipal@hotmail.com>
2023-05-30 02:06:00 -0300

    aaaw

--- usuarios/funcoes/etapasEvento.php
+++ usuarios/funcoes/etapasEvento.php
@@ -15,6 +15,7 @@ if(($pedido_selecionado) AND ($pedido_selecionado->rowCount() != 0) ){
         <h6>Pedido #<?php echo $row_Pedido['id']; ?></h6>
         <h6>Data Agendada: <?php echo date('d/m/Y',  strtotime($row_Pedido['dataagendada'])); ?></h6>
         <h6>Etapa #<?php echo $row_Pedido['payments_statu_Id']; ?></h6>
+        <h6>Aceito #<?php if($row_Pedido['confirmado'] == 1){echo "Evento Confirmado"; }else{ echo "Aguarde o guia aceitar !"; } ?></h6>
         <hr>
 
     </div>
