2b879ff5eba2aefb19b7a152717a667d60b3d97a
turistus <desconectaprincipal@hotmail.com>
2023-05-30 02:06:50 -0300

    a

--- usuarios/funcoes/etapasEvento.php
+++ usuarios/funcoes/etapasEvento.php
@@ -11,11 +11,11 @@ if(($pedido_selecionado) AND ($pedido_selecionado->rowCount() != 0) ){
     extract($row_Pedido);
     ?>
 
-    <div class="container" style="display:inline;">
+    <div class="col-12" style="display:inline;">
         <h6>Pedido #<?php echo $row_Pedido['id']; ?></h6>
         <h6>Data Agendada: <?php echo date('d/m/Y',  strtotime($row_Pedido['dataagendada'])); ?></h6>
         <h6>Etapa #<?php echo $row_Pedido['payments_statu_Id']; ?></h6>
-        <h6>Aceito #<?php if($row_Pedido['confirmado'] == 1){echo "Evento Confirmado"; }else{ echo "Aguarde o guia aceitar !"; } ?></h6>
+        <h6> #<?php if($row_Pedido['confirmado'] == 1){echo "Evento Confirmado"; }else{ echo "Aguarde o guia aceitar !"; } ?></h6>
         <hr>
 
     </div>
