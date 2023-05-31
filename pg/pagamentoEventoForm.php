0d4b870def535a87b4d013cf6b10456b1c55f875
turistus <desconectaprincipal@hotmail.com>
2023-05-31 15:22:42 -0300

    aaaa

--- pg/pagamentoEventoForm.php
+++ pg/pagamentoEventoForm.php
@@ -268,7 +268,7 @@ include_once './configPicPay.php';
                    <?php
 
                    if($valorSelecionado){
-                    $busca_custo = mysqli_query($conex, "SELECT *, valores.total AS totCusto, valores.vagas AS nDeVagas FROM valores WHERE valores.id = $valorSelecionado GROUP BY valores.idEvento ");
+                    $busca_custo = mysqli_query($conex, "SELECT *, valores.total AS totCusto, valores.vagas AS nDeVagas FROM valores WHERE valores.id = $valorSelecionado ");
                     $linhaDeValores = mysqli_fetch_assoc($busca_custo);
 
                    ?>
