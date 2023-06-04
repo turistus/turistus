33460a1d9c6e8512df6e03fa6950e487615e0c29
turistus <desconectaprincipal@hotmail.com>
2023-06-04 16:53:57 -0300

    a

--- guias/listaEventoPago.php
+++ guias/listaEventoPago.php
@@ -10,12 +10,14 @@ $Uid = $_SESSION['user_id'];
          <div class="row" style="margin: 10px;">
                 <!-- Titulo-->
                 <div class="row" style="padding-left: 50px;  margin-bottom: 50px; background: url(../images/bussola.jpg) repeat-x top center;">
+                    <div class="col-12">
                         <h1 style="padding-top: 10px;">Pedidos Completos</h1>
-
+                    </div>
                 </div>
-</br>
-                <p> Seus pedidos realizados por turistas e seus comentarios </p>
 
+                <div class="col-12">
+                    <p> Seus pedidos realizados por turistas e seus comentarios. </p>
+                </div>
              <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12" style=" min-height: 50px;">
                   <?php
                         $query_products =
@@ -30,18 +32,11 @@ $Uid = $_SESSION['user_id'];
                             phone,
                             pay.custoPedido AS valor,
                         eventos.id AS idE,
-                            eventos.nome AS nE,
-                            eventos.idPt AS idPt,
-
-                            eventos.idGuia,
-                        pontosturisticos.id,
-                            pontosturisticos.image AS i
-                        FROM payments_picpays AS pay
+                            eventos.nome AS nE
+                         FROM payments_picpays AS pay
 
                             INNER JOIN eventos ON eventos.id = pay.product_id
-                            INNER JOIN pontosturisticos ON pontosturisticos.id = eventos.idPt
                             INNER JOIN servicos ON servicos.id = pay.guiaId
-
                             WHERE pay.guiaId = $Uid AND pay.payments_statu_Id >= 5";
 
                          $result_products = $conn->prepare($query_products);
