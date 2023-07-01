dfc8431ef8d87d665d4317d06ab39757bfb4da6d
turistus <desconectaprincipal@hotmail.com>
2023-07-01 05:21:12 -0300

    aa

--- guias/painelGuia.php
+++ guias/painelGuia.php
@@ -58,7 +58,7 @@ $emailusuario = $_SESSION['user_email'];
                                 </div>
 
                               <h4 class="display-6 mt-3 mb-2">Bem vindo, Guia <?php echo $nome?> </h4>
-                              <p> Guia: <b>00<?php echo $Uid?></b> Plano: <b><span style="border-radius: 10px; border: black solid 1px; padding:3px;">BÁSICO</span></b> </p> <a href="upgradePlanos.php"><button class="btn btn-sucess">  +Upgrade</button></a>
+                              <p> Guia: <b>00<?php echo $Uid?></b> Plano: <b><span style="border-radius: 10px; border: black solid 1px; padding:3px;">BÁSICO</span></b> <a href="upgradePlanos.php"><button class="btn btn-sucess">  +Upgrade</button></a></p>
                               <?php if(isset($_SESSION['msg'])){
                                       echo $_SESSION['msg'];
                                       unset($_SESSION['msg']);
