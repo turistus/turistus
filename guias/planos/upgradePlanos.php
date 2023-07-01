5ce707a94b032d758ad346676f35707cf1ca5e48
turistus <desconectaprincipal@hotmail.com>
2023-07-01 16:47:59 -0300

    aaa

--- guias/planos/upgradePlanos.php
+++ guias/planos/upgradePlanos.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="shortcut icon" href="../images/logooriginal.png" >
+        <link rel="shortcut icon" href="../../images/logooriginal.png" >
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" >
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"/>
